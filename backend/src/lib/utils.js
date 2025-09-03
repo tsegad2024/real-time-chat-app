d2c0312d4bf244734c9894ad1b9a8752fa73747c
Tsegaye Duga <tsegad2024@gmail.com>
2025-09-03 13:36:26 +0300

    clear log from utils

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,14 +7,12 @@ export const generateToken = async (userId, res) => {
             expiresIn: '1hr'
         })
 
-        console.log("check 3")
         res.cookie("jwt", token, {
             maxAge: 1*60*60,
             httpOnly: true, 
             sameSite: 'strict',
             secure: process.env.NODE_ENV !== "development"? true: false
         })
-        console.log("check 4")
         return token;
     } catch (error) {
         return res.status(400).json({message: "Error while generating token"})
